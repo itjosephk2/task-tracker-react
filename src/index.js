ec641ed8feebe9cc0216e4729d08ab3f435a0fc1
itjosephk2 <josephkeane996@gmail.com>
2025-04-16 09:00:56 +0100

    feat: last try to get am I responsve to  work

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from './context/ThemeContext'; // ✅ add this line
+import { ThemeProvider } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider> {/* ✅ wrap App here */}
+    <ThemeProvider>
       <App />
     </ThemeProvider>
   </React.StrictMode>
